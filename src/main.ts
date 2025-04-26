5e2e63500138ec2121da93a2bdcb350551a3b429
Pierre-Adrien Vasseur <pierreadrien.vasseur@gmail.com>
2025-04-26 10:50:19 +0200

    🔖 v1.1.1

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ export default class CopilotPlugin extends Plugin {
 	copilotAgent: CopilotAgent;
 	private cmExtensionManager: ExtensionManager;
 	private eventManager: EventManager;
-	version = "1.1.0";
+	version = "1.1.1";
 	tabSize = Vault.DEFAULT_TAB_SIZE;
 
 	async onload() {
