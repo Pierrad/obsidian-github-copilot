81eae56baf7b668d1306ca50fc68482ab0451661
nikita novikov <69369034+tr3bleee@users.noreply.github.com>
2025-05-29 17:04:00 +0300

    Fix value of Claude Sonnet 4

--- src/copilot-chat/store/slices/message.tsx
+++ src/copilot-chat/store/slices/message.tsx
@@ -49,7 +49,7 @@ export const defaultModels: ModelOption[] = [
 	{ label: "Claude 3.7 Sonnet Thinking", value: "claude-3.7-sonnet-thought" },
 	{ label: "Claude 3.7 Sonnet", value: "claude-3.7-sonnet" },
 	{ label: "Claude 3.5 Sonnet", value: "claude-3.5-sonnet" },
-	{ label: "Claude Sonnet 4", value: "claude-sonnet-4-0" },
+	{ label: "Claude Sonnet 4", value: "claude-sonnet-4" },
 	{ label: "Gemini 2.0 Flash", value: "gemini-2.0-flash-001" },
 	{ label: "Gemini 2.5 Pro", value: "gemini-2.5-pro-preview-03-25" },
 ];
